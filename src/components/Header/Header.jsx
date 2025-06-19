9d9f75abd0fcc3ef8a68613b7ecb2155eb8967e8
kalocesa <kalocesa95@gmail.com>
2025-06-19 16:28:32 -0600

    add: estructura de navbar

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import fondo from "@images/back-fantasia.png";
+
+function Header() {
+  return (
+    <header id="hero">
+      <img src={fondo} alt="" />
+    </header>
+  );
+}
+
+export default Header;
