2d99b8913a3a5c8e2d32fc57a44d330b19ce2896
kalocesa <kalocesa95@gmail.com>
2025-06-21 09:28:36 -0600

    styles: modal del libro

--- src/components/Book/BookGrid/BookGrid.jsx
+++ src/components/Book/BookGrid/BookGrid.jsx
@@ -1,14 +1,24 @@
 import BookCard from "../BookCard/BookCard";
+import BookModal from "../BookCard/BookModal/BookModal";
 import books from "../../../data/book.json";
+import { useState } from "react";
 
 function BookGrid() {
+  const [selectedBook, setSelectedBook] = useState(null);
+  const handleDiscover = (book) => {
+    setSelectedBook(book);
+  };
+
   return (
     <>
-      <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
+      <div className="grid grid-cols-2 sm:grid-cols-4 lg:grid-cols-6 gap-5 p-5">
         {books.map((book) => (
-          <BookCard key={book.id} book={book} />
+          <BookCard key={book.id} book={book} onDiscover={handleDiscover} />
         ))}
       </div>
+      {selectedBook && (
+        <BookModal book={selectedBook} onClose={() => setSelectedBook(null)} />
+      )}
     </>
   );
 }
