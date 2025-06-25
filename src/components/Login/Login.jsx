c6519dbaf006b9b1f9f6fbe071e9486879bfaa15
kalocesa <kalocesa95@gmail.com>
2025-06-24 19:42:41 -0600

    styles: en register y login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import background from "@images/back-black.png";
+import "../Login/Login.css";
 
 export const Login = ({ handleRegister }) => {
   const [email, setEmail] = useState("");
@@ -19,42 +20,45 @@ export const Login = ({ handleRegister }) => {
         className="absolute inset-0 w-full h-screen object-cover opacity-30 z-0"
       />
       <div className="relative z-20 bg-black/90 rounded-2xl p-8 max-w-[550px] w-full">
-        <h1 className="text-3xl md:text-4xl text-center mb-6 py-5">
+        <h1 className="text-3xl md:text-4xl text-center mb-6 py-5 login__title">
           PuntoEncuentro te espera!
         </h1>
         <form onSubmit={handleSubmit} className="pb-10">
           <fieldset className="flex flex-col gap-4 mb-6">
-            <label className="text-sm font-medium">
+            <label className="text-sm font-medium login__label">
               Correo electrónico:
               <input
                 type="email"
                 placeholder="Correo electrónico"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
-                className="text-black mt-1 px-4 py-2 border rounded-full w-full bg-white placeholder-black focus:outline-none focus:ring-2 focus:ring-indigo-400"
+                className="text-black mt-1 px-4 py-2 border rounded-full w-full bg-white placeholder-black focus:outline-none focus:ring-2 focus:ring-cyan-100 login__input"
               />
             </label>
-            <label className="text-sm font-medium">
+            <label className="text-sm font-medium login__label">
               Contraseña:
               <input
                 type="password"
                 placeholder="Contraseña"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
-                className="text-black mt-1 px-4 py-2 border rounded-full w-full bg-white placeholder-black focus:outline-none focus:ring-2 focus:ring-indigo-400"
+                className="text-black mt-1 px-4 py-2 border rounded-full w-full bg-white placeholder-black focus:outline-none focus:ring-2 focus:ring-cyan-100 login__input"
               />
             </label>
           </fieldset>
           <button
             type="submit"
-            className="w-3/4 block mx-auto bg-[#9e0059] py-2 px-4 rounded-full hover:bg-[#9e0059]/30 transition duration-300 cursor-pointer"
+            className="w-3/4 block mx-auto bg-[#9e0059] py-2 px-4 rounded-full hover:bg-[#9e0059]/30 transition duration-300 text-xl cursor-pointer"
           >
             Inicia sesión
           </button>
         </form>
-        <p className="text-center text-xs md:text-sm text-white mt-4">
+        <p className="text-center text-xs md:text-sm text-white mt-4 login__link">
           ¿Aun no eres miembro?{" "}
-          <Link to="/signup" className="text-[#ff5400] hover:underline">
+          <Link
+            to="/signup"
+            className="text-[#ff5400] hover:underline login__link-signin"
+          >
             Regístrate aquí
           </Link>
         </p>
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import background from "@images/back-black.png";
+import "../Register/Register.css";
 
 export const Register = ({ handleRegister }) => {
   const [name, setName] = useState("");
@@ -20,52 +21,55 @@ export const Register = ({ handleRegister }) => {
         className="absolute inset-0 w-full h-screen object-cover opacity-30 z-0"
       />
       <div className="relative z-20 bg-black/90 rounded-2xl p-8 max-w-[550px] w-full">
-        <h1 className="text-3xl md:text-4xl text-center mb-6 py-5">
+        <h1 className="text-3xl md:text-4xl text-center mb-6 py-5 register__title">
           Únete a PuntoEncuentro!
         </h1>
         <form onSubmit={handleSubmit} className="pb-10">
           <fieldset className="flex flex-col gap-4 mb-6">
-            <label className="text-sm font-medium">
+            <label className="text-sm font-medium register__label">
               Nombre:
               <input
                 type="text"
                 placeholder="Tu nombre"
                 value={name}
                 onChange={(e) => setName(e.target.value)}
-                className="text-black mt-1 px-4 py-2 border rounded-full w-full bg-white placeholder-black focus:outline-none focus:ring-2 focus:ring-indigo-400"
+                className="text-black mt-1 px-4 py-2 border rounded-full w-full bg-white placeholder-black focus:outline-none focus:ring-2 focus:ring-cyan-100 register__input"
               />
             </label>
-            <label className="text-sm font-medium">
+            <label className="text-sm font-medium register__label">
               Correo electrónico:
               <input
                 type="email"
                 placeholder="Correo electrónico"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
-                className="text-black mt-1 px-4 py-2 border rounded-full w-full bg-white placeholder-black focus:outline-none focus:ring-2 focus:ring-indigo-400"
+                className="text-black mt-1 px-4 py-2 border rounded-full w-full bg-white placeholder-black focus:outline-none focus:ring-2 focus:ring-cyan-100 register__input"
               />
             </label>
-            <label className="text-sm font-medium">
+            <label className="text-sm font-medium register__label">
               Contraseña:
               <input
                 type="password"
                 placeholder="Contraseña"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
-                className="text-black mt-1 px-4 py-2 border rounded-full w-full bg-white placeholder-black focus:outline-none focus:ring-2 focus:ring-indigo-400"
+                className="text-black mt-1 px-4 py-2 border rounded-full w-full bg-white placeholder-black focus:outline-none focus:ring-2 focus:ring-cyan-100 register__input"
               />
             </label>
           </fieldset>
           <button
             type="submit"
-            className="w-3/4 block mx-auto bg-[#ff5400] py-2 px-4 rounded-full hover:bg-[#ff5400]/30 transition duration-300 cursor-pointer"
+            className="w-3/4 block mx-auto bg-[#ff5400] py-2 px-4 rounded-full hover:bg-[#ff5400]/30 transition duration-300 cursor-pointer text-xl"
           >
             Regístrate
           </button>
         </form>
-        <p className="text-center text-xs md:text-sm text-white mt-4">
+        <p className="text-center text-xs md:text-sm text-white mt-4 register__link">
           ¿Ya eres miembro?{" "}
-          <Link to="/signin" className="text-[#9e0059] hover:underline">
+          <Link
+            to="/signin"
+            className="text-[#9e0059] hover:underline register__link-signin"
+          >
             Inicia sesión aquí
           </Link>
         </p>
